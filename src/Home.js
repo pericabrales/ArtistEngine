7c2d46f3ae8f1a180902b84ee274a429bfff3b1b
Kate Galle <gallek@oregonstate.edu>
2021-03-09 16:34:07 -0800

    Background and more nav styling

--- src/Home.js
+++ src/Home.js
@@ -10,41 +10,10 @@ class Home extends React.Component{
     render(){
         return(
             <div>
-                <div id="nav">
-                    <div id="siteTitle">
-                        <h1 id="title"> Artist Engine </h1>
-                        <h2 id="title-text"> Start Exploring Music... </h2>
-                    </div>
-                <div>
-                    <ul id="navhome">
-                        <li>
-                            <NavLink to='/' id="navStyle" exact activeClassName="active-link">
-                                Home
-                            </NavLink> 
-                        </li>
-                        <li>
-                            <NavLink to='/songs' id="navStyle" activeClassName="active-link">
-                                Songs
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/albums' id="navStyle" activeClassName="active-link">
-                                Albums
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/artists' id="navStyle" activeClassName="active-link">
-                                Artists
-                            </NavLink>
-                        </li>
-                     </ul>  
-                </div>
+                <Nav/>
                
             </div>
-                
-             
-
-            </div>
+  
         )
     }
 }
